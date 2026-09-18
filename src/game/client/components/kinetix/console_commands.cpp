d1228cb86da2c785031eb6b39a6fb77d9b4a4f24
tamk283 <tamk283@users.noreply.github.com>
2026-09-18 19:55:31 +0400

    Fix console_commands.cpp: broken string literals, add base/io.h include, replace nonexistent GetCvarValue with PossibleConfigVariables+Serialize, fix cvar names

--- src/game/client/components/kinetix/console_commands.cpp
+++ src/game/client/components/kinetix/console_commands.cpp
@@ -1,6 +1,9 @@
 #include <game/client/components/kinetix/kinetix.h>
 #include <game/client/components/kinetix/kinetix_internal.h>
 
+#include <base/io.h>
+
+#include <engine/shared/config.h>
 #include <engine/storage.h>
 
 std::string CBotNet::ReplacePlaceholders(const std::string &Cmd, int DummyIndex, CGameClient *pGame)
@@ -177,6 +180,22 @@ void CBotNet::ConPfPaste(IConsole::IResult *pResult, void *pUserData)
 // Saves all Kx cvars to a config file and loads them back.
 // =========================================================
 
+struct SCvarSaveCtx
+{
+	const char *pName;
+	bool Found;
+	char aLine[512];
+};
+
+static void CvarSaveCallback(const SConfigVariable *pVariable, void *pUserData)
+{
+	SCvarSaveCtx *pCtx = (SCvarSaveCtx *)pUserData;
+	if(pCtx->Found || str_comp(pVariable->m_pScriptName, pCtx->pName) != 0)
+		return;
+	pCtx->Found = true;
+	pVariable->Serialize(pCtx->aLine, sizeof(pCtx->aLine));
+}
+
 void CBotNet::ConSaveSettings(IConsole::IResult *pResult, void *pUserData)
 {
 	CBotNet *pSelf = (CBotNet *)pUserData;
@@ -193,11 +212,11 @@ void CBotNet::ConSaveSettings(IConsole::IResult *pResult, void *pUserData)
 
 	const char *Cvars[] = {
 		"kx_aimbot", "kx_triggerbot",
-		"kx_attack", "kx_stand", "kx_auto_aim", "kx_auto_fire", "kx_auto_hook",
+		"kx_attack", "kx_stand", "kx_autoaim", "kx_autofire", "kx_autohook",
 		"kx_move", "kx_rescue", "kx_rescue_all", "kx_smart_detect", "kx_smart_rescue",
 		"kx_kill_frz", "kx_atk_main", "kx_hammer", "kx_avoid_freeze", "kx_pf_hook",
 		"kx_kinodynamic", "kx_atk_pathfinder", "kx_atk_pathfinder_rays", "kx_atk_pathfinder_rays_dist",
-		"kx_atk_pathfinder_snap", "kx_atk_pathfinder_sps", "kx_simulate_players",
+		"kx_atk_pathfinder_snap", "kx_atk_pathfinder_sps", "kx_pf_simulate_players",
 		"kx_fire_dist", "kx_hook_dist", "kx_rescue_radius", "kx_target_dist",
 		"kx_main_dist", "kx_stand_dist", "kx_main_stand_dist", "kx_target_all",
 		"kx_main", "kx_copy_moves", "kx_copy_target_id", "kx_random_aim",
@@ -206,7 +225,7 @@ void CBotNet::ConSaveSettings(IConsole::IResult *pResult, void *pUserData)
 		"kx_esp", "kx_esp_box", "kx_esp_hitbox", "kx_esp_style", "kx_esp_speed",
 		"kx_fake_aim", "kx_fake_aim_mode", "kx_fake_aim_speed", "kx_fake_aim_distance",
 		"kx_fake_aim_show_for_me",
-		"kx_avoid_freeze", "kx_baf_avoid_freeze", "kx_baf_avoid_teleport",
+		"kx_baf_avoid_freeze", "kx_baf_avoid_teleport",
 		"kx_baf_avoid_death", "kx_baf_direction", "kx_baf_jump", "kx_baf_hook",
 		"kx_baf_aim", "kx_baf_fov", "kx_baf_angles", "kx_baf_silent",
 		"kx_baf_ticks", "kx_baf_trigger_ticks",
@@ -218,20 +237,30 @@ void CBotNet::ConSaveSettings(IConsole::IResult *pResult, void *pUserData)
 		"kx_jet_ride", "kx_jet_ride_radius",
 		"kx_kino_candidates", "kx_kino_ticks", "kx_kino_hook_angles",
 		"kx_kino_cache_ticks", "kx_kino_show_path", "kx_kino_show_field", "kx_kino_aggressive",
-		"kx_trajectory",
+		"kx_show_trajectory",
 	};
 
 	int count = sizeof(Cvars) / sizeof(Cvars[0]);
+	int saved = 0;
 	for(int i = 0; i < count; i++)
 	{
-		char aBuf[512];
-		str_format(aBuf, sizeof(aBuf), "%s %s
-", Cvars[i], pSelf->Console()->GetCvarValue(Cvars[i]));
-		io_write(File, aBuf, str_length(aBuf));
+		SCvarSaveCtx Ctx;
+		Ctx.pName = Cvars[i];
+		Ctx.Found = false;
+		Ctx.aLine[0] = 0;
+		pSelf->ConfigManager()->PossibleConfigVariables(Cvars[i], CFGFLAG_CLIENT, CvarSaveCallback, &Ctx);
+		if(!Ctx.Found)
+		{
+			dbg_msg("kinetix", "skipping unknown cvar '%s'", Cvars[i]);
+			continue;
+		}
+		io_write(File, Ctx.aLine, str_length(Ctx.aLine));
+		io_write_newline(File);
+		saved++;
 	}
 
 	io_close(File);
-	dbg_msg("kinetix", "Saved %d settings to %s", count, pFilename);
+	dbg_msg("kinetix", "Saved %d settings to %s", saved, pFilename);
 }
 
 void CBotNet::ConLoadSettings(IConsole::IResult *pResult, void *pUserData)
@@ -263,8 +292,7 @@ void CBotNet::ConLoadSettings(IConsole::IResult *pResult, void *pUserData)
 	while(*pLine)
 	{
 		char *pEnd = pLine;
-		while(*pEnd && *pEnd != '
-')
+		while(*pEnd && *pEnd != '\n')
 			pEnd++;
 		if(*pEnd)
 			*pEnd++ = '\0';
